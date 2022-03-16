64d263fafff2dd5f8aeb8f6edf72938bf45dde2c
Dayo Adewuyi <92578713+dayolovesruby@users.noreply.github.com>
2022-03-16 23:46:26 +0100

    Create Blocktoken.sol

---
+++ Blocktoken.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+
+/// @custom:security-contact dayoadewuyi@protonmail.com
+contract Blocktoken is ERC20, Ownable {
+    constructor() ERC20("Blocktoken", "BTK") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    uint256 price = 1000;
+
+    function buytoken(address receiver, uint256 amount) public onlyOwner {
+        _mint(receiver, (amount * price));
+    }
+}
