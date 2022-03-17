69db3b49a912712ada5a88b81bc2adac634e5683
Dayo Adewuyi <92578713+dayolovesruby@users.noreply.github.com>
2022-03-17 02:37:32 +0100

    Add files via upload

---
+++ BlockCats.sol
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: MIT
+
+/// @title Base64
+/// @author Brecht Devos - <brecht@loopring.org>
+/// @notice Provides a function for encoding some bytes in base64
+library Base64 {
+    string internal constant TABLE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+
+    function encode(bytes memory data) internal pure returns (string memory) {
+        if (data.length == 0) return '';
+        
+        // load the table into memory
+        string memory table = TABLE;
+
+        // multiply by 4/3 rounded up
+        uint256 encodedLen = 4 * ((data.length + 2) / 3);
+
+        // add some extra buffer at the end required for the writing
+        string memory result = new string(encodedLen + 32);
+
+        assembly {
+            // set the actual output length
+            mstore(result, encodedLen)
+            
+            // prepare the lookup table
+            let tablePtr := add(table, 1)
+            
+            // input ptr
+            let dataPtr := data
+            let endPtr := add(dataPtr, mload(data))
+            
+            // result ptr, jump over length
+            let resultPtr := add(result, 32)
+            
+            // run over the input, 3 bytes at a time
+            for {} lt(dataPtr, endPtr) {}
+            {
+               dataPtr := add(dataPtr, 3)
+               
+               // read 3 bytes
+               let input := mload(dataPtr)
+               
+               // write 4 characters
+               mstore(resultPtr, shl(248, mload(add(tablePtr, and(shr(18, input), 0x3F)))))
+               resultPtr := add(resultPtr, 1)
+               mstore(resultPtr, shl(248, mload(add(tablePtr, and(shr(12, input), 0x3F)))))
+               resultPtr := add(resultPtr, 1)
+               mstore(resultPtr, shl(248, mload(add(tablePtr, and(shr( 6, input), 0x3F)))))
+               resultPtr := add(resultPtr, 1)
+               mstore(resultPtr, shl(248, mload(add(tablePtr, and(        input,  0x3F)))))
+               resultPtr := add(resultPtr, 1)
+            }
+            
+            // padding with '='
+            switch mod(mload(data), 3)
+            case 1 { mstore(sub(resultPtr, 2), shl(240, 0x3d3d)) }
+            case 2 { mstore(sub(resultPtr, 1), shl(248, 0x3d)) }
+        }
+        
+        return result;
+    }
+}
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+
+contract BlockCats is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    
+    constructor() ERC721 ("BlockCats", "BCT") {
+
+    }
+
+    function mintNFT() public {
+        
+    uint256 newItemId = _tokenIds.current();
+
+    _safeMint(msg.sender, newItemId);
+    string memory json = string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"name": "Valo", "description": "A valNFT collection", "image":"',
+                                "https://ipfs.io/ipfs/QmaAjEJnvsz4n8oeVjmEHgogSEexF8yQoqAmF9ZLH9Df3u/1.svg",
+                                '"}'
+                            )
+                        )
+                    )
+                )
+            );
+    _setTokenURI(newItemId, json);
+
+    _tokenIds.increment();
+    }
+}
